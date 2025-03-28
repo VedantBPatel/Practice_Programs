a0c15a28a4fe0f4d232a44f43a169ee91df0161d
VedantPatel <vedantpatel1969@gmail.com>
2025-03-28 16:57:16 +0530

    Factorial_Finding

--- Set-1/Practical-06.py
+++ Set-1/Practical-06.py
@@ -1,4 +1,4 @@
 def factorial(n):
     return 1 if n==0 else n*factorial(n-1)
-number=int(input("Enter a number: \n"))
+number=int(input("Enter a number you want factorial of: \n"))
 print(f"Factorial of {number} is {factorial(number)}")
