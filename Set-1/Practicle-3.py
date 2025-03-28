f2d7ef5c1719c1afdd1f7113d176ee56d2d51857
VedantPatel <vedantpatel1969@gmail.com>
2025-03-28 15:30:17 +0530

    Password_Generator

---
+++ Set-1/Practicle-3.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password():
+    length = int(input("Enter the desired length of your password : \n"))
+    characters = string.ascii_letters+string.digits5
+    password = ''.join(random.choice(characters) for i in range(length))
+    print(f"Generated Password is: {password}\n")
+generate_password()
+
