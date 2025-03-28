d11fca6ce956c29fab1831db83b255a07a40f6db
VedantPatel <vedantpatel1969@gmail.com>
2025-03-28 15:21:41 +0530

    Rectangle_Measurement

---
+++ Set-1/Practical-2.py
@@ -0,0 +1,10 @@
+def rectangle_calculator():
+    length = float(input("Enter the length: \n"))
+    width = float(input("Enter the width: \n"))
+    perimeter = 2*(length+width)
+    area = (length*width)
+    print(f"Area: {area:.2f}, Perimeter: {perimeter:.2f} \n")
+rectangle_calculator()
+
+
+  
